55a0e52a76aa2a37422dd1b49c991a161f533b62
2005lakshmi <145553662+2005lakshmi@users.noreply.github.com>
2025-01-09 19:41:16 +0530

    Update check.py

--- check.py
+++ check.py
@@ -258,8 +258,9 @@ def default_page():
        
     else:
         st.info("No subjects available at the moment.")
-st.markdown("<hr style = 'border : 1px solid gray;'>", unsafe_allow_html = True)
-st.write("Contact us on WhatsApp:[WhatsApp](http://wa.me/919964924820)")
+    st.markdown("<hr style = 'border : 1px solid gray;'>", unsafe_allow_html = True)
+    st.markdown("<hr style = 'border : 1px solid gray;'>", unsafe_allow_html = True)
+    st.write("Contact us on WhatsApp:[WhatsApp](http://wa.me/919964924820)")
 
 
 # Main function for page navigation
